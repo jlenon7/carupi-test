128346191c08bc7c65d34ae7c5167e6079986caf
João Lenon <lenonsec7@gmail.com>
2021-02-24 16:34:17 -0300

    feat: Swagger ApiProperties

--- app/Controllers/Validators/CreateCarValidator.ts
+++ app/Controllers/Validators/CreateCarValidator.ts
@@ -1,32 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { CreateCarDto } from 'app/Contracts/Dtos/CreateCarDto'
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class CreateCarValidator extends CreateCarDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   brand: string
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   model: string
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   version: string
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   year: number
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   mileage: number
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   gearboxType: string
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   price: number
 }
--- app/Controllers/Validators/UpdateCarValidator.ts
+++ app/Controllers/Validators/UpdateCarValidator.ts
@@ -1,32 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { UpdateCarDto } from 'app/Contracts/Dtos/UpdateCarDto'
 import { IsNumber, IsOptional, IsString } from 'class-validator'
 
 export class UpdateCarValidator extends UpdateCarDto {
   @IsString()
   @IsOptional()
+  @ApiProperty()
   brand: string
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   model: string
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   version: string
 
   @IsNumber()
   @IsOptional()
+  @ApiProperty()
   year: number
 
   @IsNumber()
   @IsOptional()
+  @ApiProperty()
   mileage: number
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   gearboxType: string
 
   @IsNumber()
   @IsOptional()
+  @ApiProperty()
   price: number
 }
